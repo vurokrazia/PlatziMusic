4843d92e4f21216a191bf22e9a007042f9ee4bea
jesus <jesus.alberto.vk@gmail.com>
2020-01-14 18:34:53 -0600

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-
-  constructor() {}
-
+  slideOpt = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centerdSlides: true,
+    speed: 600
+  };
+  slides = [
+    { title: "This title", subtitle: "Subtitle", description: "Aqui descriotion", icon: "play" },
+    { title: "This title", subtitle: "Subtitle", description: "Aqui descriotion", icon: "play" },
+    { title: "This title", subtitle: "Subtitle", description: "Aqui descriotion", icon: "play" }
+  ];
+  constructor() { }
 }
